f8b93fd86b4e830958bebf362b1f5e4e4f7a6782
Lux92 <lucianolimina92@gmail.com>
2017-06-09 10:26:38 +0200

    altre robe

--- src/networkLayer.cc
+++ src/networkLayer.cc
@@ -130,26 +130,26 @@ MACAddress NetworkLayer::resolveMACAddress(const char *addr) {
  */
 
 
-void dijsktraAlg(double graph[numNodi][numNodi],inst src) {
+void dijsktraAlg(double graph[numNodi][numNodi],ins src) {
 
-    //Funtion that implements Dijkstra's single source shortest path algorithm
+    //Function that implements Dijkstra's single source shortest path algorithm
     // for a graph represented using adjacency matrix representation
     {
-         int dist[V];     // The output array.  dist[i] will hold the shortest
+         int dist[numNodi];     // The output array.  dist[i] will hold the shortest
                           // distance from src to i
 
-         bool sptSet[V]; // sptSet[i] will true if vertex i is included in shortest
+         bool sptSet[numNodi]; // sptSet[i] will true if vertex i is included in shortest
                          // path tree or shortest distance from src to i is finalized
 
-         // Initialize all distances as INFINITE and stpSet[] as false
-         for (int i = 0; i < V; i++)
+         // Initialize all distances as INFINITE and stpSet[] as false , INT_MAX
+         for (int i = 0; i < numNodi; i++)
             dist[i] = INT_MAX, sptSet[i] = false;
 
          // Distance of source vertex from itself is always 0
          dist[src] = 0;
 
          // Find shortest path for all vertices
-         for (int count = 0; count < V-1; count++)
+         for (int count = 0; count < numNodi-1; count++)
          {
            // Pick the minimum distance vertex from the set of vertices not
            // yet processed. u is always equal to src in first iteration.
@@ -159,12 +159,12 @@ void dijsktraAlg(double graph[numNodi][numNodi],inst src) {
            sptSet[u] = true;
 
            // Update dist value of the adjacent vertices of the picked vertex.
-           for (int v = 0; v < V; v++)
+           for (int v = 0; v < numNodi; v++)
 
              // Update dist[v] only if is not in sptSet, there is an edge from
              // u to v, and total weight of path from src to  v through u is
              // smaller than current value of dist[v]
-             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MIN
                                            && dist[u]+graph[u][v] < dist[v])
                 dist[v] = dist[u] + graph[u][v];
          }
@@ -176,13 +176,17 @@ void dijsktraAlg(double graph[numNodi][numNodi],inst src) {
 
 // A utility function to find the vertex with minimum distance value, from
 // the set of vertices not yet included in shortest path tree
-int minDistance(int dist[], bool sptSet[])
+// Deve restiture il nodo con RSSI maggiore!!!
+// mmm ha come ingresso un vettore dist[] sto vettore deve contere i pesi (RSSI)
+// Altra nota da noi dijikstra funziona al contrario quindi setto a -infinito il valore iniziale
+
+int minDistance(int dist[], bool sptSet[], int numNodi)
 {
    // Initialize min value
-   int min = INT_MAX, min_index;
+   int min = INT_MIN, min_index;
 
-   for (int v = 0; v < V; v++)
-     if (sptSet[v] == false && dist[v] <= min)
+   for (int v = 0; v < numNodi; v++)
+     if (sptSet[v] == false && dist[v] >= min)
          min = dist[v], min_index = v;
 
    return min_index;
