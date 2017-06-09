7b35fa1c74e9d217ce977669f9d08e39a46724df
Lux92 <lucianolimina92@gmail.com>
2017-06-09 09:53:15 +0200

    Aggiunti commenti su dijkstra- Fase pre scleraggio

--- src/networkLayer.cc
+++ src/networkLayer.cc
@@ -96,13 +96,36 @@ MACAddress NetworkLayer::resolveMACAddress(const char *addr) {
     return MACAddress::UNSPECIFIED_ADDRESS;
 }
 
-/*
-int NetworkLayer::resolveAddress(MACAddress *maddres){
-    int ind;
-   uint64 ind64;
-    ind64 = maddres->getInt();
-     ind = ind64;
-     return ind;
 
-}
-*/
+/*
+ *  Algoritmo di Dijkstra
+ *
+ *  Si deve generare un SPT (Short path three) dando come ingresso una radice ( un grafo suppongo)
+ *  Si costruiscono due insiemi: un insieme "A" contiene di nodi che appartengono al SPT mentre l'altro insieme
+ *   "B" contiene  i nodi esclusi.
+ *  Ad ogni passo dell'algoritmo si sceglie un nodo dall'insieme B che abbia la distanza minima  e lo si
+ *  inserisce nell'insieme A.
+ *
+ *  PASSI:
+ *  1) Creare un insieme sptSet che raccoglie i vertici inclusi nell' SPT ( vertici = nodi )
+ *  2) Viene assegnato ad ogni nodo una distanza. Inizialmente tutti i vertici hanno distanza infinita
+ *     tranne il nodo di partenza che avrà distanza 0
+ *  3) Finchè sptSet non ha tutti i vertici
+ *     --> Viene preso un nodo u che non è contenuto in sptSet e ha la distanza minima
+ *     --> Viene aggiunto u al sptSet
+ *     --> Viene aggiornata la distanza di tutti i nodi adiacenti:
+ *          Per aggiornare la distanza per ogni nodo adiacente v se la somma tra la distanza tra u e il peso del
+ *           lato (il collegamento) è minore della distanza di v viene aggiornata la distanza di v
+ *
+ *
+ *  L' sptSet è inizialmente vuoto e le distanze dei vertici sono: {0, INF, INF, INF, INF, INF, INF, INF},
+ *  Adesso viene preso il vertice con distanza minore ovvero --> 0
+ *  Quindi  ora l'sptSet conterrà  solo il nodo sorgente. Successivamente vengo aggiornate le distanze tra il vertice 0
+ *  e quelli adiacenti.
+ *  L'algoritmo continua iterando questa operazione: quindi viene preso un nodo adiacente che ha il peso minore,
+ *  vengono calcolati i pesi dei nodi adiacenti ( viene fatta la somma dei nodi adiacenti a quello di partenza )
+ *   e vengono aggiornati gli altri
+ *
+ *   Adesso però non dobbiamo raggionare con le distanze ma con L'RSSI!!
+ */
+
